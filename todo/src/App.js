6e16036fe675cabfdb38785857a13e104b6afbec
Gideon <gideon@Mac-1s-MBP.home>
2020-03-16 18:16:15 -0400

    initializing useReducer, todo with useState hooks  and handleChanges

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
+const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+const reducer = (state, action) => {
+
+
+}
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [todo, setTodo] = useState('');
+
+  const handleChanges = e => {
+    setTodo(e.target.value)
+  }
+
+
 
   return (
     <div className="App">
+
+      <h2>Welcome to your Reducer-Todo App</h2>
+      <form>
+        <div>
+          <input type='text' value={todo} onChange={handleChanges}/>
+          <button>Add Todo</button>
+        </div>
+        <div>
+          <div>
+            {}
+          </div>
+          <button>Clear Todo</button>
+        </div>
+          
+      </form>
       
       <TodoForm />
       <TodoList />
